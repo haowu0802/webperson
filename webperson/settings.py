13fb3d1ba7d88e0385c9606a22c925bdf6efd407
Hao Wu <haowu0802@gmail.com>
2017-01-29 23:03:44 -0800

    fix static path for heroku

--- webperson/settings.py
+++ webperson/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))  # for heroku
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -122,5 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # static files path: BASE_PATH/static/*
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')  # for heroku
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # static files path: BASE_PATH/static/*
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
